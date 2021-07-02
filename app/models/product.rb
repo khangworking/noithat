3bea221f9390846e0647835f7172ba854d7cafd6
Khang <khang.job@gmail.com>
2021-07-02 22:33:14 +0700

    Add annotate gem

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  active      :boolean          default(FALSE)
+#  description :string
+#  image       :string
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Product < ApplicationRecord
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  active      :boolean          default(FALSE)
+#  description :string
+#  image       :string
+#  name        :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
