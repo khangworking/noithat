31b1f9c1f4846bcaa3f2c3361244231095838cae
Khang <khang.job@gmail.com>
2021-07-10 10:29:00 +0700

    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :admin do
     resources :products
+
+    root to: 'dashboard#index'
   end
 end
